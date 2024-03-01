b70c5399f0b808b0678962c107d073fea4771fe1
Leeyoujin00 <cymn6032@gmail.com>
2024-03-01 17:11:47 +0900

    study: chap 07

---
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/domain/controller/BookForm.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop.domain.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class BookForm {
+
+    private Long id;
+    private int price;
+    private String name;
+    private int stockQuantity;
+    private String author;
+    private String isbn;
+}
